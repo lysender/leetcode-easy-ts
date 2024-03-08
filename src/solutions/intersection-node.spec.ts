6252a23f6f27f54cfdc661881ddba907d287bd15
Lysender <lysender@darkstar-win>
2024-03-08 10:08:29 +0800

    fix: add intersection node solution

---
+++ src/solutions/intersection-node.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest';
+import { createList } from '../types/list-node';
+import { getIntersectionNode } from './intersection-node';
+
+describe('intersection-node', () => {
+  describe('test1 - with intersect', () => {
+    it('should pass', async () => {
+      const list1 = createList([4, 1, 8, 4, 5]);
+      const list2 = createList([5, 6, 1]);
+      // Then join the last node of list 2 to the other node
+      list2[2].next = list1[2];
+
+      expect(getIntersectionNode(list1[0], list2[0])).toEqual(list1[2]);
+    });
+  });
+
+  describe('test2 - with intersect', () => {
+    it('should pass', async () => {
+      const list1 = createList([1, 9, 1, 2, 4]);
+      const list2 = createList([3]);
+      // Then join the last node of list 2 to the other node
+      list2[0].next = list1[3];
+
+      expect(getIntersectionNode(list1[0], list2[0])).toEqual(list1[3]);
+    });
+  });
+
+  describe('test3 - not intersect', () => {
+    it('should pass', async () => {
+      const list1 = createList([2, 6, 4]);
+      const list2 = createList([1, 5]);
+
+      expect(getIntersectionNode(list1[0], list2[0])).toEqual(null);
+    });
+  });
+});
--- src/solutions/intersection-node.ts
+++ src/solutions/intersection-node.ts
@@ -4,5 +4,26 @@ export function getIntersectionNode(
   headA: ListNode | null,
   headB: ListNode | null,
 ): ListNode | null {
+  // First try, use a set for headA, then try each node in headB if they matches in the hash map
+  if (headA === null || headB === null) {
+    return null;
+  }
+
+  const set = new Set<ListNode>();
+  // Process headA
+  let current: ListNode | null = headA;
+  while (current !== null) {
+    set.add(current);
+    current = current.next;
+  }
+
+  // Find intersect in headB
+  current = headB;
+  while (current !== null) {
+    if (set.has(current)) {
+      return current;
+    }
+    current = current.next;
+  }
   return null;
 }
