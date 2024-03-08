614931ea17feb84482cd1928bd0038c9b24479d4
Lysender <lysender@darkstar-win>
2024-03-08 10:44:06 +0800

    fix: add intersectio node using two pointer technique, slow

--- src/solutions/intersection-node.ts
+++ src/solutions/intersection-node.ts
@@ -27,3 +27,39 @@ export function getIntersectionNode(
   }
   return null;
 }
+
+export function getIntersectionNodeTwoPointer(
+  headA: ListNode | null,
+  headB: ListNode | null,
+): ListNode | null {
+  // Use two points and loop when at the same time
+  // When any of the pointer reaches the end, reset pointer to head
+  // Once they have cycled, they are equidistant with each other
+  // They either met at intersection or both meet the end, meaning, no intersection
+  let a: ListNode | null = headA;
+  let b: ListNode | null = headB;
+
+  if (headA === null || headB === null) {
+    return null;
+  }
+
+  while (a !== b) {
+    a = a?.next ?? null;
+    b = b?.next ?? null;
+
+    // Found it
+    if (a === b) {
+      return a;
+    }
+
+    if (a === null) {
+      // Reset to head
+      a = headA;
+    }
+
+    if (b === null) {
+      b = headB;
+    }
+  }
+  return a;
+}
